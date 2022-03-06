d4867b5533eeece0a41f9a40fc04b1dfd968bd27
INAPOLI17 <apolinarar72@gmail.com>
2022-03-06 16:22:24 -0400

    Create Musica.cs
    clase modelo

---
+++ Musica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana7
+{
+    public class Musica
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string artista { get; set; }
+        public string albúm { get; set; }
+
+    }
+}
