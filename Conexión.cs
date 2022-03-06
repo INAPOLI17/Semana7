c1b0af3e96dbb1ae59ed9086f420e666223a4083
INAPOLI17 <apolinarar72@gmail.com>
2022-03-06 16:23:00 -0400

    Update Conexión.cs
    CONEXION CON PROBLEMA AL CONECTAR LA DB

--- Conexión.cs
+++ Conexión.cs
@@ -10,12 +10,30 @@ namespace Semana7
 {
     public class Conexión
     {
-        SqlConnection cone = new SqlConnection(@"Data Source=DESKTOP-QUV81SK;Initial Catalog=Musica;Integrated Security=True");
+        SqlConnection cone = new SqlConnection(@"Data Source=(local);Initial Catalog=Musica;Integrated Security=True");
 
         public DataTable mostrar(string strsql)
         {
             DataTable dt = new DataTable();
             SqlDataAdapter ds = new SqlDataAdapter(strsql, cone);
+            ds.Fill(dt);
+            return dt; 
+        }
+
+        public void inupmusic(Musica newmusic)
+        {
+            SqlCommand cmd = new SqlCommand("sp_inup_musica", cone);
+            cone.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@codigo", newmusic.id);
+            cmd.Parameters.AddWithValue("@nombre", newmusic.nombre);
+            cmd.Parameters.AddWithValue("@album", newmusic.albúm);
+            cmd.Parameters.AddWithValue("@artista", newmusic.artista);
+
+            cmd.ExecuteNonQuery();
+            cone.Close();
+
         }
 
     }
